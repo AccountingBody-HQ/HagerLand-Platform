1cfbe8fbbbdd68d827a5d14e0a190f23f729d86d
Acctnbody <acctn.body@gmail.com>
2026-07-25 23:46:17 +0000

    style(branding): refine motto to smaller cleaner tagline

--- components/SiteNav.tsx
+++ components/SiteNav.tsx
@@ -28,7 +28,7 @@ export function SiteNav() {
           <Logo className='w-7 h-7' />
           <span className='flex flex-col leading-none'>
             <span className='font-brand font-extrabold text-[22px] text-ink tracking-tight' translate="no">HagerLand<span style={{fontSize:'0.55em',display:'inline-block',position:'relative',top:'-0.85em',opacity:1,marginLeft:'2px',fontWeight:400}}>®</span></span>
-            <span className='text-[8.5px] font-bold uppercase tracking-[0.14em] text-green mt-[3px]' translate="no">Connect and Cooperate</span>
+            <span className='text-[7px] font-semibold uppercase tracking-[0.1em] text-green mt-[1px]' translate="no">Connect and Cooperate</span>
           </span>
         </Link>
 
