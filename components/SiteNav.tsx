df7883ced28dac50089be7163ea1a286a7bd4bf3
Acctnbody <acctn.body@gmail.com>
2026-07-25 22:49:21 +0000

    style(branding): increase trademark symbol size for visibility

--- components/SiteNav.tsx
+++ components/SiteNav.tsx
@@ -26,7 +26,7 @@ export function SiteNav() {
         {/* Logo */}
         <Link href='/' className='flex items-center gap-2 shrink-0' translate="no">
           <Logo className='w-7 h-7' />
-          <span className='font-bold text-base text-ink tracking-tight' translate="no">HagerLand<sup className='text-[0.6em] font-normal align-super'>&reg;</sup></span>
+          <span className='font-bold text-base text-ink tracking-tight' translate="no">HagerLand<sup className='text-[0.72em] font-normal align-super opacity-80'>&reg;</sup></span>
         </Link>
 
         {/* Desktop nav links */}
